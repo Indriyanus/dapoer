2ed670735f1f4832e7d507826c07ef7b84b44daf
ginda <baginda.s.hidayat@astrapay.com>
2024-09-12 12:46:15 +0700

    ok

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
     images: {
       domains: ['localhost'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'w6fiv6w6vqb3bfam.public.blob.vercel-storage.com',
+          port: '',
+        },
+      ],
     }
   }
   
